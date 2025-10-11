917750d7dbb42de4963ed4b2b6ea6bf071433333
Lily <20535390+kitten-lily@users.noreply.github.com>
2025-10-11 08:14:23 +0200

    Build with dagger

--- .dagger/src/bluefin/main.py
+++ .dagger/src/bluefin/main.py
@@ -8,10 +8,10 @@ from dagger import DefaultPath, dag, function, object_type
 class Bluefin:
     source: Annotated[dagger.Directory, DefaultPath(".")]
 
-    def _base_container(self) -> dagger.Container:
+    def _base_container(self, src: str) -> dagger.Container:
         """Internal helper to add common cache mounts."""
         return (
-            dag.container().from_("ghcr.io/ublue-os/bluefin:stable-daily")
+            dag.container().from_(src)
             .with_mounted_cache("/var/roothome", dag.cache_volume("var-roothome"))
             .with_mounted_cache("/var/cache", dag.cache_volume("var-cache"))
             .with_mounted_cache("/var/log", dag.cache_volume("var-log"))
@@ -19,11 +19,23 @@ class Bluefin:
             .with_mounted_cache("/var/lib/dnf", dag.cache_volume("var-lib-dnf"))
         )
 
+    def _with_labels(self, container: dagger.Container, owner: str, image: str, logo: str) -> dagger.Container:
+        return (
+            container
+            .with_label("io.artifacthub.package.readme-url", f"https://raw.githubusercontent.com/{owner}/{image}/refs/heads/main/README.md")
+            .with_label("io.artifacthub.package.homepage-url", f"https://github.com/${owner}/{image}")
+            .with_label("io.artifacthub.package.documentation-url", f"https://github.com/{owner}/{image}/blob/main/README.md")
+            .with_label("io.artifacthub.package.source-url", f"https://github.com/{owner}/{image}")
+            .with_label("io.artifacthub.package.license", "Apache-2.0")
+            .with_label("io.artifacthub.package.logo-url", logo)
+            .with_label("io.artifacthub.package.prerelease", "false")
+        )
+
     @function
-    def bluefin_container(self) -> dagger.Container:
+    def bluefin_container(self, src: str) -> dagger.Container:
         """Returns a container with the bluefin image"""    
         return (
-            self._base_container()
+            self._base_container(src)
             .with_directory("/", self.source.directory("system_files"))
             .with_exec(["mkdir", "-p", "/var/opt"])
             .with_exec(["dnf", "-y", "copr", "enable", "gvalkov/vicinae"])
@@ -50,3 +62,15 @@ class Bluefin:
             .with_exec(["bootc", "container", "lint"])
         )
 
+    @function
+    def bluefin_publish(self, src: str, owner: str, image: str, logo: str, tags: list[str]) -> list[str]:
+        """Publish container to ghcr.io"""    
+        container = self._with_labels(
+            self.bluefin_container(src),
+            owner,
+            image,
+            logo
+        )
+
+        return tags
+        
