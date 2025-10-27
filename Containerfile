421ac42d650cbf4c22940c8c4333348bb923b81c
Lily <20535390+kitten-lily@users.noreply.github.com>
2025-10-27 09:53:50 +0100

    Beta (#10)
    * Beta
    
    * saf
    
    * wrong copr
    
    * Configure auth
    
    * Tag as latest

--- Containerfile
+++ Containerfile
@@ -3,7 +3,7 @@ FROM scratch AS ctx
 COPY build_files /
 
 # Base Image
-FROM ghcr.io/ublue-os/bluefin-dx:stable-daily@sha256:5a28db53c06d36f225e99eb66c852585793efffaa246a4f14a9014dfa4f19bae
+FROM ghcr.io/ublue-os/bluefin:beta
 
 ### MODIFICATIONS
 COPY system_files/ /
@@ -15,7 +15,6 @@ RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     --mount=type=cache,dst=/var/lib/xkb \
     --mount=type=tmpfs,dst=/tmp \
     . /ctx/build.sh
-
         
 ### LINTING
 ## Verify final image and contents are correct.
--- build_files/build.sh
+++ build_files/build.sh
@@ -7,29 +7,22 @@ mkdir -p /var/opt
 
 ### Install packages
 
-dnf -y copr enable gvalkov/vicinae
-dnf -y config-manager setopt rpmfusion*.enabled=1 terra.enabled=1
+#dnf -y copr enable gvalkov/vicinae
+dnf -y copr enable scottames/ghostty
 
 dnf -y install \
-	coolercontrol \
-    headsetcontrol \
-	liquidctl \
-    lutris \
-    openrgb \
-    https://vencord.dev/download/vesktop/amd64/rpm \
-    vicinae \
-    warp-terminal
+    ghostty
+#    vicinae
 
-dnf -y copr disable gvalkov/vicinae
-dnf -y config-manager setopt rpmfusion*.enabled=0 terra.enabled=0 warpdotdev.enabled=0
-
+#dnf -y copr disable gvalkov/vicinae
+dnf -y copr disable scottames/ghostty
 
 ### Move opt stuff
-rm /usr/bin/warp-terminal
-ln -s /opt/warpdotdev/warp-terminal/warp /usr/bin/warp-terminal
-
 mv /var/opt /usr/share/factory/var/opt
 
 ### Enable services
 systemctl enable podman.socket
 systemctl enable podman-restart.service
+
+### Configure authentication
+authselect select sssd with-pam-u2f with-silent-lastlog with-mdns4
